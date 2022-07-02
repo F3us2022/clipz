67c60d7bee2cdaa29ef4c747b353cbb5413c2a78
F3us2022 <arsenal0288@gmail.com>
2022-07-01 21:36:36 -0400

    v1.3 commit

--- src/app/shared/directives/event-blocker.directive.ts
+++ src/app/shared/directives/event-blocker.directive.ts
@@ -5,6 +5,7 @@ import { HostListener } from '@angular/core';
   selector: '[app-event-blocker]',
 })
 export class EventBlockerDirective {
+  //preventing the video to upload on a separate browser window
   @HostListener('drop', ['$event'])
   @HostListener('dragover', ['$event'])
   public handleEvent(event: Event) {
